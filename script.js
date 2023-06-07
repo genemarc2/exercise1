9afe6f1c95972199c8a4a97c6548d2ed9455e369
genemarc2 <134124194+genemarc2@users.noreply.github.com>
2023-06-07 23:20:28 +0800

    Add files via upload

---
+++ script.js
@@ -0,0 +1,7 @@
+alert('Click "OK" to load.')
+
+confirm('Are you sure you wanna go on?')
+
+let result = prompt('Im gonna ask again, are you sure? type "Aye" if you are sure');
+
+console.log(result);
